209ee08b5b593c56eb6c9d3d34f6c8a8b9053af2
alejandro-reyes <alereyes1793@gmail.com>
2016-03-06 22:33:56 -0600

    smtp pba

--- app/Config/email.php
+++ app/Config/email.php
@@ -36,49 +36,11 @@
  */
 class EmailConfig {
 
-	public $default = array(
-		'transport' => 'Mail',
-		'from' => 'you@localhost',
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
-	);
-
 	public $smtp = array(
 		'host' => 'ssl://smtp.gmail.com',
 		'port' => 465,
 		'username' => 'ucrtcu593@gmail.com',
 		'password' => 'admintcu593'
-	);
-
-	public $fast = array(
-		'from' => 'you@localhost',
-		'sender' => null,
-		'to' => null,
-		'cc' => null,
-		'bcc' => null,
-		'replyTo' => null,
-		'readReceipt' => null,
-		'returnPath' => null,
-		'messageId' => true,
-		'subject' => null,
-		'message' => null,
-		'headers' => null,
-		'viewRender' => null,
-		'template' => false,
-		'layout' => false,
-		'viewVars' => null,
-		'attachments' => null,
-		'emailFormat' => null,
-		'transport' => 'Smtp',
-		'host' => 'localhost',
-		'port' => 25,
-		'timeout' => 30,
-		'username' => 'user',
-		'password' => 'secret',
-		'client' => null,
-		'log' => true,
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
 	);
 
 }
